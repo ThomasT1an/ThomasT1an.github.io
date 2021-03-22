aad0470cd55f12346c3ba9a753b0792a3b4ac97c
tianzhongyi <tianzhongyi@wecash.net>
2021-03-22 18:44:31 +0800

    1

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package tzy.code;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodeApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CodeApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package tzy.code;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CodeApplicationTests {
+class DemoApplicationTests {
 
     @Test
     void contextLoads() {
