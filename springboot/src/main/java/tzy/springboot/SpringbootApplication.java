731d966dabdbee5c2825c01093b756e762ab2c8e
ThomasT1an <492126033@qq.com>
2021-07-18 14:41:43 +0800

    1

--- springboot/src/main/java/tzy/springboot/SpringbootApplication.java
+++ springboot/src/main/java/tzy/springboot/SpringbootApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args
+        );
     }
 
 }
